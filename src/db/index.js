730a6dc79f680fe9dfb1fa086218e524f7090a52
Laged <parkkila.matti@gmail.com>
2017-04-28 21:34:38 +0900

    Add initial DB implementation

---
+++ src/db/index.js
@@ -0,0 +1,94 @@
+import Cloudant from 'cloudant';
+import fs from 'fs';
+import { logger } from '../utils/logger';
+
+// Database configuration
+const dbName = 'asdasd';
+let dbUrl;
+
+function getDbUrl(jsonData) {
+  const vcapServices = JSON.parse(jsonData);
+  // Find Cloudant service in VCAP_SERVIES
+  for (const vcapService in vcapServices) {
+    if (vcapService.match(/cloudant/i)) {
+      return vcapServices[vcapService][0].credentials.url;
+    }
+  }
+  return '';
+}
+
+if (process.env.VCAP_SERVICES) { // IBM bluemix host
+  dbUrl = getDbUrl(process.env.VCAP_SERVICES);
+} else { // Local host
+  dbUrl = getDbUrl(fs.readFileSync('vcap-local.json', 'utf-8'));
+}
+
+// FIXME: initialize URL properly
+
+// Initialize cloudant connection
+const cloudant = Cloudant(dbUrl);
+cloudant.db.create(dbName, (err) => {
+  if (err) {
+    logger.info(`Could not create db ${dbName} - does it already exist?`);
+  } else {
+    logger.info(`Database initialized: ${dbName}`);
+  }
+});
+
+// Use proper database
+const db = cloudant.use(dbName);
+db.list((err, allDbs) => {
+  const dbstring = JSON.stringify(allDbs, null, 2);
+  logger.info(`${dbName} databases ${dbstring}`);
+});
+
+
+export function getSampleData() {
+  return [
+    { timestamp: 10.00, weight: 1.4 },
+    { timestamp: 40.00, weight: 2.7 },
+    { timestamp: 60.00, weight: 3.4 },
+  ];
+}
+
+export function getData() {
+  logger.info('getData');
+  return new Promise((resolve, reject) => {
+    if (db) {
+      db.fetch({}, (err, docs) => {
+        if (err) {
+          reject(err);
+        }
+        if (docs.rows.length === 0) {
+          resolve([]);
+        }
+        const filtered = docs.rows
+          // FIXMID FILTER .filter(...)
+          .map(d => ({ timestamp: d.doc.timestamp, weight: d.doc.weight }));
+        logger.info(`docs: ${JSON.stringify(filtered, null, 2)}`);
+        resolve(filtered);
+      });
+    }
+  });
+}
+
+export function getDataBetween(startTime, endTime) {
+  logger.info(`getDataBetween ${startTime} - ${endTime}`);
+  return new Promise((resolve, reject) => {
+    if (db) {
+      db.fetch({}, (err, docs) => {
+        if (err) {
+          reject(err);
+        }
+        if (docs.rows.length === 0) {
+          resolve([]);
+        }
+        const filtered = docs.rows
+          .map(d => ({ timestamp: d.doc.timestamp, weight: d.doc.weight }))
+          .filter(d => d.timestamp > startTime && d.timestamp < endTime);
+        logger.info(`docs: ${JSON.stringify(filtered, null, 2)}`);
+        resolve(filtered);
+      });
+    }
+  });
+}
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import cors from 'cors';
 import express from 'express';
 import morgan from 'morgan';
 import { logger, logStream } from './utils/logger';
+import { getData, getDataBetween } from './db/index';
+
+logger.info(getData);
 
 const app = express();
 const port = 3000;
@@ -17,13 +20,27 @@ app
   res.json({ status: 'up' });
 })
 .get('/weightData', (req, res) => {
-  const data = [
-    { timestamp: 10.00, weight: 1.4 },
-    { timestamp: 40.00, weight: 2.7 },
-    { timestamp: 60.00, weight: 3.4 },
-  ];
-  logger.info('/weightData', data);
-  res.json(data);
+  // Fetch all data
+  getData()
+  .then((data) => {
+    logger.info('/weightData', data);
+    res.json(data);
+  });
+})
+.get('/weightDataBetween', (req, res) => {
+  logger.info(req.query);
+  const startTime = parseInt(req.query.start, 10);
+  const endTime = parseInt(req.query.end, 10);
+  logger.info(`weightDataBetween ${startTime} - ${endTime}`);
+  if (isNaN(startTime) || isNaN(endTime)) {
+    return res.status(400).json({ error: 'startTime and/or endTime parameters invalid' });
+  }
+  // Fetch data between given time
+  return getDataBetween(startTime, endTime)
+  .then((data) => {
+    logger.info('/weightData', data);
+    return res.json(data);
+  });
 });
 
 app.listen(port, () => logger.info(`server running on ${port}`));
