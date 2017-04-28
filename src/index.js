a448454094c420261467cbfbef38b6ae01c73d51
Laged <parkkila.matti@gmail.com>
2017-04-28 18:41:05 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.json({ status: 'up' });
+});
+
+app.listen(port, () => console.log(`server running on ${port}`));
