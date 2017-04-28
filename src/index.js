4d57b8d525b117c67edcccc2df23450890d329cf
Laged <parkkila.matti@gmail.com>
2017-04-28 18:49:07 +0900

    Add mock endpoint for weigth data

--- src/index.js
+++ src/index.js
@@ -5,6 +5,13 @@ const port = 3000;
 
 app.get('/', (req, res) => {
   res.json({ status: 'up' });
+})
+.get('/weightData', (req, res) => {
+  res.json([
+    { timestamp: 10.00, weight: 1.4 },
+    { timestamp: 40.00, weight: 2.7 },
+    { timestamp: 60.00, weight: 3.4 }],
+  );
 });
 
 app.listen(port, () => console.log(`server running on ${port}`));
