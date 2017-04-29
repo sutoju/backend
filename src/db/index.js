90aea214b45923a9583262f16148b2e0e422c3b2
Henri Nurmi <henri.nurmi@kapsi.fi>
2017-04-30 07:42:20 +0900

    Fix foods experies times

--- src/db/index.js
+++ src/db/index.js
@@ -39,8 +39,8 @@ export function initWeights() {
 export function initItems() {
   const now = Math.floor(Date.now() / 1000.0);
   const applePrototype = { type: 'apple', added: now - (24 * 3600), expires: now + (48 * 3600) };
-  const ricePrototype = { type: 'rice', added: now - (240 * 3600), expires: now + (170 * 3600) };
-  const honeyPrototype = { type: 'honey', added: now - (120 * 3600), expires: now + (320 * 3600) };
+  const ricePrototype = { type: 'rice', added: now - (5 * 24 * 3600), expires: now + (270 * 24 * 3600) };
+  const honeyPrototype = { type: 'honey', added: now - (5 * 24 * 3600), expires: now + (350 * 24 * 3600) };
   const addItem = (body) => {
     fetch('https://sutoju-logic.eu-gb.mybluemix.net/initItem', {
       method: 'POST',
